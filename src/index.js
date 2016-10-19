d7329ed71d52d08d365d7708203b8a73dda2ac6d
Brandon <railsdev14@gmail.com>
2016-10-19 16:44:09 -0400

    First commit

---
+++ src/index.js
@@ -0,0 +1,16 @@
+// Libs
+import React from 'react';
+import { render } from 'react-dom';
+import { Router, Route } from 'react-router';
+
+// CSS
+import './css/style.css';
+
+// Components
+import App from './components/App';
+
+// Render
+render(
+  <App />,
+  document.getElementById('root')
+);
